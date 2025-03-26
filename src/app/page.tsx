aed99be8591798a75fb53f59b24647d8eda10a51
Chris Sato <cmsato09.git@gmail.com>
2025-03-26 12:25:16 -0700

    Add navbar and sheet menu

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,16 +12,8 @@ import {
 export default function Home() {
   return (
     <div className='m-4'>
-      <SignedOut>
-        <SignInButton mode="modal">
-          <Button>Sign in</Button>
-        </SignInButton>
-      </SignedOut>
-
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-      <DarkModeToggle />
+      <h1>Home Page</h1>
+      
     </div>
   );
 }
