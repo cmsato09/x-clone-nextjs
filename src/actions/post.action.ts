2c9a9e988705ab335f3090cfe64c0b7e158f6d68
Chris Sato <cmsato09.git@gmail.com>
2025-04-14 15:44:32 -0700

    Add CreatePost to home page

---
+++ src/actions/post.action.ts
@@ -0,0 +1,23 @@
+"use server";
+
+import prisma from "@/lib/prisma";
+import { getDbUserId } from "./user.actions";
+import { revalidatePath } from "next/cache";
+
+export async function createPost(content: string, imageUrl:string){
+  try {
+    const userId = await getDbUserId();
+    const post = await prisma.post.create({
+      data: {
+        content,
+        image: imageUrl,
+        authorId: userId,
+      }
+    })
+    revalidatePath("/") // purges cache for the home page
+    return {success: true, post}
+  } catch (error) {
+    console.error("Failed to create post:", error)
+    return { success: false, error: "Failed to create post"}
+  }
+}
